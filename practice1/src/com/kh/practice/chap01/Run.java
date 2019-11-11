c1b8a45b0c15d3738afcb0f0ae867173cf3859f7
kwong1110 <kwong1110@gmail.com>
2019-11-12 00:43:59 +0900

    11월 2주차 연습 (조건문)

---
+++ practice1/src/com/kh/practice/chap01/Run.java
@@ -0,0 +1,28 @@
+package com.kh.practice.chap01;
+
+public class Run {
+
+	public static void main(String[] args) {
+//		ControlPractice cp = new ControlPractice();
+//		cp.practice1();
+//		ControlPractice cp = new ControlPractice();
+//		cp.practice2();
+//		ControlPractice cp = new ControlPractice();
+//		cp.practice3();
+//		ControlPractice cp = new ControlPractice();
+//		cp.practice4();
+//		ControlPractice cp = new ControlPractice();
+//		cp.practice5();
+//		ControlPractice cp = new ControlPractice();
+//		cp.practice6();
+//		ControlPractice cp = new ControlPractice();
+//		cp.practice7();
+//		ControlPractice cp = new ControlPractice();
+//		cp.practice8();
+//		ControlPractice cp = new ControlPractice();
+//		cp.practice9();
+		ControlPractice cp = new ControlPractice();
+		cp.practice10();
+	}
+
+}
